a97b3463d957acdcc6740fc83f49a4f20ed579d2
jpswag <johnpan@umich.edu>
2024-09-27 22:37:53 -0400

    ready for submission

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 curl -X POST http://localhost:8000/walker/interact \
      -H "Authorization: Bearer $1" \
-     -H "Content-Type: application/json" \
-     -d '{"message": "I am having major back pain, what can i do", "session_id": "123"}'
+     --header "Content-Type: application/json" \
+     --data '{"message": "I am having major back pain, what can i do", "session_id": "123"}'
